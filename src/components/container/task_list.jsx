1903f473072b62e8e098e9f9e0cf54eb565d18da
rubendario981 <rubendario981@hotmail.com>
2022-07-18 19:52:15 -0500

    Added a  table with the task list, also added Bootstrap icons

--- src/components/container/task_list.jsx
+++ src/components/container/task_list.jsx
@@ -5,8 +5,12 @@ import TaskComponent from '../pure/forms/task'
 
 const TaskListComponent =() => {
   const defaultTask = new Task('Example', 'Default Description', false, LEVELS.NORMAL)
+  const defaultTask2 = new Task('Example2', 'Default Description2', true, LEVELS.URGENT)
+  const defaultTask3 = new Task('Example2', 'Default Description', false, LEVELS.NORMAL)
+  const defaultTask4 = new Task('Example4', 'Default Description', true, LEVELS.BLOCKING)
+  const defaultTask5 = new Task('Example', 'Default Description', false, LEVELS.URGENT)
   
-  const [tasks, setTasks] = useState([defaultTask])
+  const [tasks, setTasks] = useState([defaultTask, defaultTask2, defaultTask3, defaultTask4,defaultTask5])
   const [loading, setLoading] = useState(true)
   
   useEffect(()=>{
@@ -21,12 +25,36 @@ const TaskListComponent =() => {
     setTasks()
   }
   return (
-    <div>
+    <div className='col-12'>
+      <div className="card">
+        <div className="card-header p3">
+          <h5 className="card-title">Your Tasks are: </h5>
+        </div>
+        <div className="card-body" data-mbd-perfect-scrollbar='true' style={{position: 'relative', heigth: '400px'}}>
+          <table>
+            <thead>
+            <tr>
+              <th scope='col'>Title</th>
+              <th scope='col'>Description</th>
+              <th scope='col'>Priority</th>
+              <th scope='col'>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+              {tasks.map((task, index)=>{
+                return(
+                  <TaskComponent key={index} task={task}></TaskComponent>
+
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
         <h2>
-            Your Tasks are: 
+
         </h2>
 
-        <TaskComponent task={defaultTask}></TaskComponent>
 
     </div>
   )
--- src/components/pure/forms/task.jsx
+++ src/components/pure/forms/task.jsx
@@ -1,28 +1,85 @@
 import PropTypes from 'prop-types'
 import { useEffect } from 'react'
 import { Task } from '../../../models/task.class'
+import { LEVELS } from '../../../models/levels.enum'
 import '../../../styles/tasks.scss'
 
-const TaskComponent = ({task})=> {
-  useEffect(()=>{
+const TaskComponent = ({ task }) => {
+  useEffect(() => {
     console.log('Task created')
-    return ()=>{
+    return () => {
       console.log(`Task: ${task.name}, is going to umount`)
 
     }
   }, [task])
+
+  const taskLevelBadge = () => {
+    switch (task.level) {
+      case LEVELS.NORMAL:
+        return (
+          <h6 className='mb-0'>
+            <span className='badge bg-primary'>
+              {task.level}
+            </span>
+          </h6>
+        )
+      case LEVELS.URGENT:
+        return (
+          <h6 className='mb-0'>
+            <span className='badge bg-warning'>
+              {task.level}
+            </span>
+          </h6>
+        )
+      case LEVELS.BLOCKING:
+        return (
+          <h6 className='mb-0'>
+            <span className='badge bg-danger'>
+              {task.level}
+            </span>
+          </h6>
+        )
+      default:
+        break;
+    }
+  }
   return (
-    <div>
-        <h2 className='task-name'>Nombre: {task.name}</h2>
-        <h3>Descripcion: {task.description}</h3>
-        <h4>Nivel: {task.level}</h4>
-        <h5>La tarea esta  {task.completed ? 'completada': 'incompleta'}</h5>
-    </div>
+    <tr className='fw-normal'>
+      <th>
+        <span className='ms-2'>
+          {task.name}
+        </span>
+      </th>
+      <td>
+        <span className='aling-middle'>{
+          task.description}
+        </span>
+      </td>
+      <td>
+        <span className='aling-middle'>
+          {taskLevelBadge()}
+        </span>
+      </td>
+      <td>
+        <span className='aling-middle'>
+          {task.completed ?
+            <i className='bi-check' style={{ fontWeigth: 'bold', color: 'green', fontSize: '32px' }}></i> :
+            <i className='bi-x-circle-fill' style={{ fontWeigth: 'bold', color: 'red', fontSize: '32px' }}></i>}
+        </span>
+        <i className="bi-trash ms-5" style={{ fontWeigth: 'bold', color: 'red', fontSize: '32px' }}></i>
+      </td>
+    </tr>
+    // <div>
+    //     <h2 className='task-name'>Nombre: {task.name}</h2>
+    //     <h3>Descripcion: {task.description}</h3>
+    //     <h4>Nivel: {task.level}</h4>
+    //     <h5>La tarea esta  {task.completed ? 'completada': 'incompleta'}</h5>
+    // </div>
   )
 }
 
 TaskComponent.propTypes = {
-    task: PropTypes.instanceOf(Task)
+  task: PropTypes.instanceOf(Task)
 }
 
 export default TaskComponent
